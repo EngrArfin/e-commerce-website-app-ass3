d23b93a29ab13dc6062443e7f3b52a6156ad631f
EngrArfin <arfin.cse.green.edu.bd@gmail.com@>
2024-05-28 15:54:15 +0600

    some update and ecommerce procudr order and product maintain

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import app from "./app";
+import config from "./app/config";
 const mongoose = require('mongoose');
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
   }
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
